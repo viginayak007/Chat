f9fcf2c52bd5b73dd0b49c1f620c04a58cbbe8a0
JPlayer <mail@julietindustries.com>
2018-04-07 12:51:01 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendfile('view/index.html');
+});
+
+users = [];
+io.on('connection', function (socket) {
+    console.log('A user connected');
+    socket.on('setUsername', function (data) {
+        console.log(data);
+
+        if (users.indexOf(data) > -1) {
+            socket.emit('userExists', data + ' username is taken! Try some other username.');
+        } else {
+            users.push(data);
+            socket.emit('userSet', {
+                username: data
+            });
+        }
+    });
+
+    socket.on('msg', function (data) {
+        //Send message to everyone
+        io.sockets.emit('newmsg', data);
+    })
+});
+
+http.listen(8080, function () {
+    console.log('listening on localhost:8080');
+});
